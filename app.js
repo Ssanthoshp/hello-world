dd34df9144ef0189e03993e5df7792b5f8433958
Ssanthoshp <153047977+Ssanthoshp@users.noreply.github.com>
2024-02-24 20:10:58 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,18 +2,32 @@
 
 console.log("I am in the HTML");
 alert("Welcome to my page!!!");
+// const theirName = getName();
+// greetuser();
 
+
+function getName() {
 const usersName = prompt("Please select one of the 5 names(Alice, Bob, Charlie, Barbie or Stranger)");
-console.log(usersName);
+return usersName;
+}
+// console.log(theirName);
 
-document.write("Hiya " +  usersName + " - Welcome to my page!!!") 
+function greetuser() {
 
+document.write("Hiya " +  theirName + " - Welcome to my page!!!") 
+}
+
+function specialMessage(usersName){
+if (usersName == ""){
+    usersName = prompt("Come on, please tell me your name");
+}
 if (usersName == "Alice") {
     let result = confirm("Do you want to proceed");
     if (result) {
         alert("You clicked Ok!")
     }
     document.write (" Happy to have you here");
+
 } else if (usersName =="Bob") {
     let result = confirm("Do you want to proceed");
     if (result) {
@@ -39,3 +53,14 @@ if (usersName == "Alice") {
     }
     document.write (" Happy to have you here");
 }
+}
+
+
+
+
+function addTwoNumbers (num1, num2) {
+    let sum = num1 + num2;
+    console.log("Sum is:", sum);
+    return sum;
+}
+
